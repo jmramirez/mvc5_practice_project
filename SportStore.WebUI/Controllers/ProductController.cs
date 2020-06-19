4f4da34c8befdd21bd780c08b130f4d9a27ac3bb
Jose Ramirez <josemramirez81@outlook.com>
2020-06-19 16:45:12 -0400

    Category-Specific Products Counts

--- SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore.WebUI/Controllers/ProductController.cs
@@ -31,7 +31,7 @@ namespace SportStore.WebUI.Controllers
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(e => e.Category == category).Count()
                 },
                 CurrentCategory = category
             };
