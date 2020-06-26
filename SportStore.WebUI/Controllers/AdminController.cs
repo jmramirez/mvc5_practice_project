608b5506f797064ccd5aa34b7e1a3b3e7adb5521
Jose Ramirez <josemramirez81@outlook.com>
2020-06-26 13:44:35 -0400

    Defining Admin Controller/Index action

---
+++ SportStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using SportStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        // GET: Admin
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
